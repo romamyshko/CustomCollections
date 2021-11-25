ce7f6897fb82778d2723a553497b96b760f9fe8f
Roman Myshko <romamyshko12@gmail.com>
2021-11-25 08:55:23 +0200

    Add project files.

---
+++ CustomCollections/Collection.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CustomCollections
+{
+    public class Collection
+    {
+        
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CustomCollections;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

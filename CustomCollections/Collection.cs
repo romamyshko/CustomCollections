130c9d40be2e4ddfa2231fb183c64f8948af282e
Roman Myshko <romamyshko12@gmail.com>
2021-11-25 14:44:04 +0200

    Add indexer, checking type, and array resize

--- CustomCollections/Collection.cs
+++ CustomCollections/Collection.cs
@@ -2,8 +2,50 @@
 
 namespace CustomCollections
 {
-    public class Collection
+    public class Collection<T>
     {
-        
+        private T[] _array;
+        private int _lastElemIndex;
+        private int _lenght;
+
+        public Collection()
+        {
+            _lastElemIndex = 0;
+            _lenght = 4;
+            _array = new T[_lenght];
+        }
+
+        private T this [int element]
+        {
+            get { return _array[element]; }
+            set { _array[element] = value;  }
+        }
+
+
+        private void Resize()
+        {
+            Array.Resize(ref _array, _lenght *= 2);
+        }
+
+        private bool IsNumericType(T item)
+        {
+            switch (Type.GetTypeCode(item.GetType()))
+            {
+                case TypeCode.Byte:
+                case TypeCode.SByte:
+                case TypeCode.UInt16:
+                case TypeCode.UInt32:
+                case TypeCode.UInt64:
+                case TypeCode.Int16:
+                case TypeCode.Int32:
+                case TypeCode.Int64:
+                case TypeCode.Decimal:
+                case TypeCode.Double:
+                case TypeCode.Single:
+                    return true;
+                default:
+                    return false;
+            }
+        }
     }
 }
